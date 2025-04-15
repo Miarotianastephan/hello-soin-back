c4d0888bb5d23d5bbcb654238c259d066a7e8d93
Miarotiana Stephan <miarotianastephan01@gmail.com>
2025-04-15 16:14:09 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 
-const userRoutes = require('./routes/user.routes');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
