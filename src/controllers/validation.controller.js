1189ccdb47f876a33282c7a7f7fd800fc7024d98
Votre Nouveau Nom <nouvel.email@exemple.com>
2025-05-02 10:50:11 +0300

    correctiftoki

--- src/controllers/validation.controller.js
+++ src/controllers/validation.controller.js
@@ -1,19 +1,21 @@
 // controllers/auth.controller.js
-require('dotenv').config();
-const EmailSenderService = require('../services/email-sender.service');  // <-- ici !
+require('dotenv').config();  // Charger .env en haut du fichier
+
+const EmailSenderService = require('../services/email-sender.service');
 const twilio = require('twilio');
 
-// const accountSid   = process.env.TWILIO_ACCOUNT_SID   || 'AC31a05c29b562d9618367cd25d47f6e72';
-// const authToken    = process.env.TWILIO_AUTH_TOKEN    || 'a2f4a64e03f84a2392d76807f293653b';
-// const twilioNumber = process.env.TWILIO_PHONE_NUMBER || '+18483530896';
-const client = twilio(accountSid, authToken);
+// Lecture depuis process.env
+const accountSid   = process.env.TWILIO_ACCOUNT_SID;
+const authToken    = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+const client       = twilio(accountSid, authToken);
 
 exports.sendCode = async (req, res) => {
   try {
     const { mail, numero } = req.body;
-    // Récupération du code grâce au return
+    // Récupération du code
     const code = await EmailSenderService.sendCode(mail);
-    
+
     if (numero) {
       await client.messages.create({
         body: `Votre code de validation est : ${code}`,
@@ -27,17 +29,16 @@ exports.sendCode = async (req, res) => {
   }
 };
 
-// Fonction pour vérifier le code de validation
 exports.verifyCode = async (req, res) => {
-    try {
-        const { mail, code } = req.body;
-        await EmailSenderService.verifyCode(mail, code);
+  try {
+    const { mail, code } = req.body;
+    await EmailSenderService.verifyCode(mail, code);
 
-        return res.status(200).json({ 
-            message: 'Code validé, vous pouvez finaliser votre inscription.',
-            status : true
-        });
-    } catch (error) {
-        return res.status(400).json({ message: error.message || 'Code non validé, veuillez entrer le bon code.' });
-    }
+    return res.status(200).json({ 
+      message: 'Code validé, vous pouvez finaliser votre inscription.',
+      status : true
+    });
+  } catch (error) {
+    return res.status(400).json({ message: error.message || 'Code non validé, veuillez entrer le bon code.' });
+  }
 };
