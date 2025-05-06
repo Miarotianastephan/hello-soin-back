5d81b7fc1891ebe61a3adf93a0dda06d0e6101ed
Votre Nouveau Nom <nouvel.email@exemple.com>
2025-05-06 03:21:08 +0300

    mmm

---
+++ src/controllers/formation.controller.js
@@ -0,0 +1,89 @@
+const FormationService = require('../services/formation.service');
+
+exports.getAllFormations= async (req, res) => {
+    try {
+        const formations = await FormationService.getAllFormationsByID(req.user.id_user);
+        return res
+            .status(200)
+            .json({
+                data: formations,
+                message: "Récupérations des formations de l'utilisateurs"
+            })
+    } catch (error) {
+        console.error(error);
+        return res
+            .status(500)
+            .json({ message: "Erreur de Récupérations des formations", error: error.message })
+    }
+}
+
+exports.createFormations = async (req, res) => {
+    try {
+        
+        // Log des données reçues
+        console.log('req.body:', req.body);
+        console.log('req.files:', req.files);
+        const { files, user } = req;
+        const {
+            institution_name,
+            obtained_at,
+            certification_name,
+            certification_number,
+            id_pract_speciality
+        } = req.body;
+
+        const pratInfo = await FormationService.findPraticienInfoById(user.id_user);
+        const id_pract_info = pratInfo.id_pract_info;
+
+        // Étape 2 : Validation des données
+        if (!institution_name || !obtained_at || !certification_name || !id_pract_info || !id_pract_speciality) {
+            return res.status(400).json({ message: 'Tous les champs requis ne sont pas fournis.' });
+        }
+
+        // Étape 3 : Construction de l'objet de données
+        const formationData = {
+            institution_name,
+            obtained_at,
+            certification_name,
+            id_pract_info,
+            certification_number: certification_number || null,
+        };
+        const support_docs = []
+
+        // Gestion des fichiers uploadés
+        if (files && files.length > 0) {
+            files.forEach(file => {
+                support_docs.push({
+                    doc_photo_url: file.filename,
+                    doc_url: file.filename
+                });
+            });
+        }
+
+        // Étape 4 : Appel au service
+        const newFormation = await FormationService.createFormation(formationData, id_pract_speciality, support_docs);
+
+        // Étape 5 : Réponse réussie
+        return res.status(201).json({
+            message: 'Formation créée avec succès.',
+            data: newFormation
+        });
+
+    } catch (error) {
+        console.error('Erreur lors de la création de la formation :', error);
+        return res.status(500).json({ message: 'Erreur interne du serveur.' });
+    }
+};
+
+exports.deleteFormation = async (req, res) => {
+    try {
+        const { id_formation } = req.body
+        await FormationService.deleteFormation(id_formation);
+        return res.status(201).json({
+            message: 'Suppression avec succès.'
+        });
+    } catch (error) {
+        console.error('Erreur lors de la suppression de la formation :', error);
+        return res.status(500).json({ message: error.message || 'Erreur interne du serveur.' });   
+    }
+}
--- src/routes/profil-praticien/profil-praticien.routes.js
+++ src/routes/profil-praticien/profil-praticien.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { middleware_global } = require('../../middlewares/auth-middlewares');
 
 const ProfilPraticienController = require('../../controllers/profil-praticien.controller');
+const FormationController = require('../../controllers/formation.controller');
 const SpecialityController = require('../../controllers/speciality.controller');
 const uploadMiddleware = require('../../middlewares/upload-middlewares');
 const uploadDocs = require('../../middlewares/upload-multiple-middlewares');
@@ -16,10 +17,9 @@ router.post("/complete-profil", uploadProfilPhoto, ProfilPraticienController.com
 router.get("/get-info-praticien", uploadMiddleware.any(), ProfilPraticienController.getInfoPraticien);
 
 // Routes gestion des formations
-router.get("/getall-formations", ProfilPraticienController.getAllFormations);
-router.post("/add-formations", uploadDocs.array('support_docs', 5), ProfilPraticienController.createFormations);
-router.post("/delete-formation", ProfilPraticienController.deleteFormation);
-// router.post("/update-formation", ProfilPraticienController.updateFormation); // ENCORE A DEVELOPPER
+router.get("/getall-formations", FormationController.getAllFormations);
+router.post("/add-formations", uploadDocs.array('support_docs', 5), FormationController.createFormations);
+router.post("/delete-formation", FormationController.deleteFormation);
 
 // Routes gestion specialités
 router.get("/get-speciality-praticien", SpecialityController.getSpecialityByPraticien);
